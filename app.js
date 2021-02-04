7241db8e42279bf68f51c5ed0c9c4c6a234d5993
DevDuki <dusan-misic@hotmail.com>
2021-02-04 23:43:19 +0100

    header part with nav finished

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('helloooo')
+const burger = document.getElementById('burger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const menu = document.querySelector('.header__menu')
+const body = document.querySelector('body')
+
+burger.addEventListener('click', () => {
+
+  if(header.classList.contains('open')){ // Burger is closing
+    header.classList.remove('open')
+    overlay.classList.remove('fade-in')
+    overlay.classList.add('fade-out')
+    menu.classList.remove('fade-in')
+    menu.classList.add('fade-out')
+    body.classList.remove('noscroll')
+  } else { // Burger is opening
+    header.classList.add('open')
+    overlay.classList.remove('fade-out')
+    overlay.classList.add('fade-in')
+    menu.classList.remove('fade-out')
+    menu.classList.add('fade-in')
+    body.classList.add('noscroll')
+  }
+})
